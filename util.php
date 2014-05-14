b21b9ca9b0902078dcd43c00f661c19e339925bd
ChargeBee <ChargeBee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-14 16:15:28 +0000

    update in webhook handling
    git-svn-id: https://plugins.svn.wordpress.org/chargebee/trunk@913983 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- util.php
+++ util.php
@@ -13,14 +13,14 @@ class chargebee_meta {
 /*
  * Gets the ChargeBee Subscription object of the user_id passed, if present at the wp_uermeta.
  */
-function chargebee_subscription_detail($userId) {
+static function chargebee_subscription_detail($userId) {
     return  get_user_meta($userId, "subscription", true);
 }
 
 /*
  * Gets the ChargeBee Customer object of the user_id passed, if present at the wp_usermeta.
  */
-function chargebee_customer_detail($userId) {
+static function chargebee_customer_detail($userId) {
     return get_user_meta($userId, "customer", true);
 }
 
@@ -29,7 +29,7 @@ function chargebee_customer_detail($userId) {
  * from the ChargeBee result object.
  * Here wordpress user_id, ChargeBee subscription id and ChargeBee customer id are same.
  */
-function update_from_result($response) {
+static function update_from_result($response) {
    if( $response->subscription() != null ) {
       $subscription = $response->subscription(); 
       update_user_meta($subscription->id, 'subscription', $subscription);
